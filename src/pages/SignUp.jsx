43323a347179373538aaa29d2e1fc80beaef9b0a
SamuelPPlab <samuelpp.s@hotmail.com>
2021-11-29 19:08:43 -0300

    Opção de voltar para a pagina de login adicionada na pagina de cadastro.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Input from '../components/Input';
 import Button from '../components/Button';
+import { Navigate } from "react-router";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
 
@@ -8,6 +10,7 @@ const SignUp = () => {
   const [email, setEmail] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [goToMain, setGoToMain] = useState(false);
 
   const nameProps = {
     id: 'nomeCompleto',
@@ -40,12 +43,27 @@ const SignUp = () => {
     type: "password",
   };
 
+  const signUpButtonProps = {
+    id: "Cadastrar",
+    name: "Cadastrar",
+    onClick: () => setGoToMain(true),
+    disable: false,
+  };
+
+  const alreadySingnedUp = <div>
+    Já possui um cadastro? <Link to="/">Login</Link>
+  </div>;
+
+  if(goToMain) return <Navigate to="/main" />;
+
   return (
     <div>
       <Input {...nameProps} />
       <Input {...emailProps} />
       <Input {...passwordInputProps} />
       <Input {...confirmPasswordProps} />
+      {alreadySingnedUp}
+      <Button {...signUpButtonProps} />
     </div>
   );
 };
