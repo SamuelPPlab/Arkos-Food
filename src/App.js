2ab583894c2f8ad3261e0f16ca6ca0cc0ddf384d
SamuelPPlab <samuelpp.s@hotmail.com>
2021-11-30 12:20:11 -0300

    Pagina de editar o perfil criada, e avisos adicionados para guiar o usuário.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import EditProfile from './pages/EditProfile';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route exact path="/signup" element={<SignUp />} />
+      <Route exact path="/edit-profile" element={<EditProfile />} />
     </Routes>
   );
 }
