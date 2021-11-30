b6d5c157b0b94231f0a49c91a58835d43fec76c3
SamuelPPlab <samuelpp.s@hotmail.com>
2021-11-29 21:05:18 -0300

    Componente Input criado.

---
+++ public/components/Input.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({ name, fieldValue = null, checked = null, onKeyUp = null,
+  setFieldValue = null, type = 'text', readOnly = false, style = null }) => {
+  const inputProps = {
+    checked,
+    value: fieldValue,
+    id: name,
+    onChange: ({ target: { value } }) => setFieldValue(value),
+    type: type,
+    readOnly: readOnly,
+    onKeyUp,
+  };
+
+  return (
+    <div>
+      <label htmlFor={ name } style={style}>
+        {name}
+        <div>
+          <input {...inputProps} />
+        </div>
+      </label>
+    </div>
+  );
+};
+
+Input.defaultProps = { type: 'text', setFieldValue: null, readOnly: false, fieldValue: null, checked: null };
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  setField: PropTypes.func,
+  type: PropTypes.string,
+  readOnly: PropTypes.bool,
+};
+
+export default Input;
