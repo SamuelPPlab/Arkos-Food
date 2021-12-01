5329574dae2e6dc12f660276050391c49dee8cb3
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-01 10:10:49 -0300

    Rota /main está com seu componente.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import EditProfile from './pages/EditProfile';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route exact path="/signup" element={<SignUp />} />
+      <Route exact path="/main" element={<MainPage />} />
       <Route exact path="/edit-profile" element={<EditProfile />} />
     </Routes>
   );
