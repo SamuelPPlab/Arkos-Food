9a5536fab75210c17f5da64f8352d7447376f92b
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-01 10:56:27 -0300

    Reducer da main page pronta.

--- src/redux/reducers/mainPageReducer.js
+++ src/redux/reducers/mainPageReducer.js
@@ -1,11 +1,20 @@
+import { ERROR_MAIN, LOADING_MAIN, SUCCESS_MAIN } from "../actions/mainPageActions";
+
 const INITIAL_STATE = {
-  loading: false,
+  loading: true,
   stock: [],
   error: '',
 };
 
 const mainPageReducer = (state = INITIAL_STATE, action) => {
+  const { stock, error } = action;
   switch (action.type) {
+    case LOADING_MAIN:
+      return { ...state, loading: true };
+    case SUCCESS_MAIN:
+      return { ...state, loading: false, stock };
+    case ERROR_MAIN:
+      return { ...state, loading: false, error };
     default:
       return state;
   }
