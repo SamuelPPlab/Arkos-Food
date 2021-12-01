bb3dc065c953af17bd3e273741da11bc3560831f
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-01 11:03:14 -0300

    Quantidade de produtos da resposta limitada a 8.

--- src/APIs/fakeProducts.js
+++ src/APIs/fakeProducts.js
@@ -1,4 +1,4 @@
 export const fetchStock = () => {
-  const url = 'https://fakestoreapi.com/products';
+  const url = 'https://fakestoreapi.com/products?limit=8';
   return fetch(url).then((r) => r.json());
 };
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { connect } from "react-redux";
+import { fetchMainData } from "../redux/actions/mainPageActions";
 
 
 const MainPage = ({ stock }) => {
+  const dispatch = useDispatch();
   console.log(stock)
+  useEffect(() => {
+    dispatch(fetchMainData());
+  }, [])
   return(
     <div>
 
