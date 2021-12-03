81607b57614b7615a2ad858fe0950f8b7ca57677
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-03 00:39:27 -0300

    Formato do card de produto no checkout feito.

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -4,31 +4,37 @@ import TrashCan from "../images/TrashCan.png";
 import { useDispatch } from "react-redux";
 import { removeItemFromCart } from "../redux/actions/checkoutActions";
 import RatingStars from "./RatingStars";
+import "../CSS/cards.css";
 
 const CartCard = ({ item }) => {
   const { title, description, image, price, id, key, rating } = item;
 
   const dispatch = useDispatch();
 
-  const trashCanIMG = <img src={TrashCan} alt="Trash Can Icon" />;
-
-  const removeItemFromCartButtonProps = {
-    name: trashCanIMG,
-    id: 'RemoveItemFromCart',
+  const trashCanIMGProps = {
     onClick: () => dispatch(removeItemFromCart(key, id)),
+    src: TrashCan,
+    alt: 'Trash Can Icon',
+    id: 'trashCanIMG',
   };
 
   return (
-    <div>
-      <img src={image} alt={title} style={{ height: '179px', width: '222px' }} />
-      <Button {...removeItemFromCartButtonProps} />
-      {title}
-      <div>
-        <RatingStars rating={rating} />
+    <div id="checkoutCardContainer">
+      <div style={{ width: '220px', height: '180px' }}>
+        <img id="checkoutImage" src={image} alt={title} style={{ height: '179px', width: '222px' }} />
       </div>
-      {description}
-      <div>
-        R$ {price}
+      <div style={{ width: '820px' }}>
+        {title}
+        <div>
+          <RatingStars rating={rating} />
+        </div>
+        {description}
+      </div>
+      <div style={{ width: '40px' }}>
+        <img {...trashCanIMGProps} />
+        <div>
+          R$ {price}
+        </div>
       </div>
     </div>
   );
